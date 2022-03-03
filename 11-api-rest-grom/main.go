3fb7cc19958dfacc8d873779ead76d8aa2924ad5
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-03-02 23:11:50 -0300

    Fin Curso de GO

---
+++ 11-api-rest-grom/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"gorm/handlers"
+	"gorm/models"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	models.MigrarUser()
+
+	//Rutas
+	mux := mux.NewRouter()
+
+	//Responder al cliente
+	mux.HandleFunc("/api/user/", handlers.GetUsers).Methods("GET")
+	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.GetUser).Methods("GET")
+	mux.HandleFunc("/api/user/", handlers.CreateUser).Methods("POST")
+	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT")
+	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE")
+
+	//Servidor
+	fmt.Println("Run server: http://localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", mux))
+
+}
---
+++ 11-api-rest-grom/models/users.go
@@ -0,0 +1,15 @@
+package models
+
+import "gorm/db"
+
+type User struct {
+	Id       int64  `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+type Users []User
+
+func MigrarUser() {
+	db.Database.AutoMigrate(User{})
+}
